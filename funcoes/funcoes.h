f528cc21a47317189f02887f965622a6eca7adb8
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2021-10-16 23:52:15 -0300

    c7 - Definindo funcao multiplicar()

--- funcoes/funcoes.h
+++ funcoes/funcoes.h
@@ -1,3 +1,5 @@
 float soma(float n1, float n2);
 
 float subtrair(float n1, float n2);
+
+float multiplicar(float n1, float n2);
--- funcoes/mat_funcoes.c
+++ funcoes/mat_funcoes.c
@@ -17,3 +17,12 @@ float subtrair(float n1, float n2)
 
     return(resultado);
 }
+
+float multiplicar(float n1, float n2)
+{
+    float resultado;
+
+    resultado = n1 * n2;
+
+    return(resultado);
+}
